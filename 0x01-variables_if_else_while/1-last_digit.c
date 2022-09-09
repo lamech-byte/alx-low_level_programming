a6978b992e5947089ccda3f28ff79cebfd0c57a4
lamech-byte <gesachmoh@gmail.com>
2022-09-09 07:15:19 -0700

    task 1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,3 +1,5 @@
+#include <stdlib.h>
+#include <time.h>
 #include <stdio.h>
 
 /**
@@ -9,11 +11,15 @@
 */
 int main(void)
 {
-char ch;
-for (ch = 'a' ; ch <= 'z' ; ch++)
-{
-	putchar(ch);
-}
-putchar('\n');
+int n;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+printf("Last digit of %d is %d", n, n % 10);
+if (n % 10 > 5)
+		printf("and is greater than 5\n");
+else if (n % 10 == 0)
+		printf("and is 0\n");
+else if (n % 10 < 6 && n % 10 !=0)
+		printf("and is less than 6 and not 0\n");
 return (0);
 }
