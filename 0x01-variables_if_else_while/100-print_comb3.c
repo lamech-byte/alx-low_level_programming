92c3916b21c54ef3f580739b665ef48b1464fef0
Lamech M <79203512+lamech-byte@users.noreply.github.com>
2022-09-09 23:58:39 +0300

    Update 100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -13,14 +13,14 @@ int main(void)
 	int ones;
 	int tens;
 
-	for (tens = '0'; tens <= '9'; tens++) /*increment tens*/
+	for (tens = '0'; tens <= '9'; tens++)
 	{
-		for (ones = (tens + 1); ones <= '9'; ones++) /*one's ten+1*/
+		for (ones = (tens + 1); ones <= '9'; ones++)
 		{
 			putchar(tens);
 			putchar(ones);
 
-			if (tens != '8' || ones != '9') /*print commas*/
+			if (tens != '8' || ones != '9')
 			{
 				putchar(',');
 				putchar(' ');
