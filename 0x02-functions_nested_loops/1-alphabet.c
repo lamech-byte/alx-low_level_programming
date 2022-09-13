d0f7c87f5fa177e1548024e377f0748525652454
lamech-byte <gesachmoh@gmail.com>
2022-09-13 02:35:09 -0700

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - prints the alphabet, in lowercase, followed by a new line
+ */
+
+int main(void)
+{
+	char ch;
+
+	ch = 'a';
+
+	while (ch <='z');
+	{
+		_putchar(ch);
+		ch++;
+	}
+
+	_putchar('\n');
+}
