be9ae1e08ebdde6811f876a2c575862b02b72291
Lamech M <79203512+lamech-byte@users.noreply.github.com>
2022-09-15 22:53:54 +0300

    Update main.h

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,10 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-int _putchar(char c);
-int _isupper(int c);
-int _isdigit(int c);
-int mul(int a, int b);
+int _putchar(char);
+int _isupper(int);
+int _isdigit(int);
+int mul(int , int);
 void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);
