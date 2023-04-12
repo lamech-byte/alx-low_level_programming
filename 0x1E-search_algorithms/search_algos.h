fcef79d16d9c29ec5e796aa6e58692edb6fa9ae7
Lamech M <79203512+lamech-byte@users.noreply.github.com>
2023-04-12 08:05:31 +0300

    Update search_algos.h

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,8 +1,12 @@
-#ifndef SEARCH_ALGOS_H
-#define SEARCH_ALGOS_H
+#ifndef SEARCH_ALGOS
+#define SEARCH_ALGOS
 
-#include <stddef.h>
+#include <stdio.h>
+#include <math.h>
 
 int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+int jump_search(int *array, size_t size, int value);
+int interpolation_search(int *array, size_t size, int value);
 
-#endif /* SEARCH_ALGOS_H */
+#endif
