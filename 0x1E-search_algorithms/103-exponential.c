810ee9fd9c64dbd356397efb7072007cb76535f2
Lamech M <79203512+lamech-byte@users.noreply.github.com>
2023-04-12 13:33:42 +0300

    Update 103-exponential.c

--- 0x1E-search_algorithms/103-exponential.c
+++ 0x1E-search_algorithms/103-exponential.c
@@ -10,33 +10,6 @@
  * Return: The first index where value is located, or -1 if not found or array is NULL
  */
 
-int _binary_search(int *array, size_t left, size_t right, int value)
-{
-	size_t i;
-
-	if (array == NULL)
-		return (-1);
-
-	while (right >= left)
-	{
-		printf("Searching in array: ");
-		for (i = left; i < right; i++)
-			printf("%d, ", array[i]);
-		printf("%d\n", array[i]);
-
-		i = left + (right - left) / 2;
-		if (array[i] == value)
-			return (i);
-		if (array[i] > value)
-			right = i - 1;
-		else
-			left = i + 1;
-	}
-
-	return (-1);
-}
-
-
 int exponential_search(int *array, size_t size, int value)
 {
 	size_t i = 0, right;
