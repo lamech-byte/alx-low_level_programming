43ef4217d256a12befcb7c4a41f51559205f0b56
Lamech M <79203512+lamech-byte@users.noreply.github.com>
2023-04-12 14:05:56 +0300

    Update 103-exponential.c

--- 0x1E-search_algorithms/103-exponential.c
+++ 0x1E-search_algorithms/103-exponential.c
@@ -1,48 +1,52 @@
 #include "search_algos.h"
+#include <stdio.h>
 
 /**
  * exponential_search - Searches for a value in a sorted array of integers
- *                      using the Exponential search algorithm
- * @array: A pointer to the first element of the array to search in
- * @size: The number of elements in array
- * @value: The value to search for
+ * using the Exponential search algorithm
  *
- * Return: The first index where value is located, or -1 if not found or array is NULL
+ * @array: Pointer to the first element of the array to search in
+ * @size: Number of elements in array
+ * @value: Value to search for
+ *
+ * Return: The first index where value is located, or -1 if not found
  */
 
 int exponential_search(int *array, size_t size, int value)
 {
+    size_t bound = 1;
+
     if (array == NULL)
         return (-1);
 
-    if (size == 0)
-        return (-1);
-
-    if (array[0] == value)
-        return (0);
-
-    size_t bound = 1;
     while (bound < size && array[bound] < value) {
-        printf("Value checked array[%ld] = [%d]\n", bound, array[bound]);
+        printf("Value checked array[%lu] = [%d]\n", bound, array[bound]);
         bound *= 2;
     }
 
+    printf("Value found between indexes [%lu] and [%lu]\n", bound / 2,
+           bound < size ? bound : size - 1);
+
     size_t left = bound / 2;
-    size_t right = bound < size ? bound : size - 1;
-    while (left <= right) {
-        size_t mid = (left + right) / 2;
+    size_t right = bound < size ? bound : size;
+
+    while (left < right) {
         printf("Searching in array: ");
         for (size_t i = left; i < right; i++) {
-            printf("%d, ", array[i]);
+            printf("%d", array[i]);
+            if (i != right - 1)
+                printf(", ");
         }
-        printf("%d\n", array[right]);
-        if (array[mid] == value) {
+        printf("\n");
+
+        size_t mid = (left + right) / 2;
+
+        if (array[mid] == value)
             return (mid);
-        } else if (array[mid] < value) {
+        else if (array[mid] < value)
             left = mid + 1;
-        } else {
-            right = mid - 1;
-        }
+        else
+            right = mid;
     }
 
     return (-1);
