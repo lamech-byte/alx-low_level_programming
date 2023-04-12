fcf103d3d66e4565eb9a21a8e4a09d01bf1e875b
Lamech M <79203512+lamech-byte@users.noreply.github.com>
2023-04-12 11:51:13 +0300

    Update 100-jump.c

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -12,56 +12,27 @@
  *         or -1 if value is not present or array is NULL
  */
 
-void print_range(size_t i, size_t j)
-{
-	printf("Value found between indexes [%lu] and [%lu]\n", i, j);
-}
-
-void print_i(int *array, size_t i)
-{
-	printf("Value checked array[%lu] = [%d]\n", i, array[i]);
-}
-
 int jump_search(int *array, size_t size, int value)
 {
-	size_t jumps = sqrt(size);
-	size_t i = 0;
-	size_t j = 0;
+	size_t i, jump, step;
 
-	if (!array)
+	if (array == NULL || size == 0)
 		return (-1);
-	while (j <= size)
-	{
-		if (j != 0)
-			print_i(array, i);
-		if (array[j] >= value)
-		{
-			print_range(i, j);
-			while (i <= j)
-			{
-				print_i(array, i);
-				if (array[i] == value)
-					return (i);
-				i++;
-			}
-			return (-1);
-		}
-		if (j + jumps + jumps > size)
-		{
-			print_i(array, j);
-			print_range(j, j + jumps);
-			while (j < size)
-			{
-				print_i(array, j);
-				if (array[j] == value)
-					return (j);
-				j++;
-			}
-			return (-1);
 
-		}
-		i = j;
-		j += jumps;
+	step = sqrt(size);
+	for (i = jump = 0; jump < size && array[jump] < value;)
+	{
+		printf("Value checked array[%ld] = [%d]\n", jump, array[jump]);
+		i = jump;
+		jump += step;
 	}
-	return (-1);
+
+	printf("Value found between indexes [%ld] and [%ld]\n", i, jump);
+
+	jump = jump < size - 1 ? jump : size - 1;
+	for (; i < jump && array[i] < value; i++)
+		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+	printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+
+	return (array[i] == value ? (int)i : -1);
 }
