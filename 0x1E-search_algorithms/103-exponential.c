3bd23e23cda1df8d2212f893a89453ae4d3189d8
Lamech M <79203512+lamech-byte@users.noreply.github.com>
2023-04-13 21:13:00 +0300

    Update 103-exponential.c

--- 0x1E-search_algorithms/103-exponential.c
+++ 0x1E-search_algorithms/103-exponential.c
@@ -69,4 +69,3 @@ int exponential_search(int *array, size_t size, int value)
 	printf("Value found between indexes [%ld] and [%ld]\n", i / 2, right);
 	return (_binary_search(array, i / 2, right, value));
 }
-
